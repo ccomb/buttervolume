f30135110d2ba21228bf964ad75fd08d33fa741c
Christophe Combelles <ccomb@free.fr>
2025-07-28 09:33:21 +0200

    adapt version of urllib3 to new version of requests-unixsocket2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     },
     install_requires=[
         "bottle",
-        "urllib3>=2.0.2,<2.3",
+        "urllib3<3.0,>=2.3.0",
         "requests-unixsocket2",
         "waitress",
         "webtest",
